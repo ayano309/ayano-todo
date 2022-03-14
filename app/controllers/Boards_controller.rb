cba40dc37c6ee8e9d7a92ab631a919819bdfe24e
綾乃 <>
2022-03-14 20:46:11 +0900

    createアクションを追加

--- app/controllers/Boards_controller.rb
+++ app/controllers/Boards_controller.rb
@@ -9,4 +9,17 @@ class BoardsController < ApplicationController
     def new
         @board = Board.new
     end
+    def create
+        @board = Board.new(board_params)
+        if @board.save
+            redirect_to board_path(@board)
+        else
+            render :new
+        end
+    end
+    
+    private
+    def board_params
+        params.require(:board).permit(:name, :description)
+    end
 end
