ff215c70af24a78bc7f1efbf56029475a6b9c9ab
綾乃 <>
2022-03-15 14:53:24 +0900

    自分の記事に対してアクションボタンを表示するように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,5 +23,9 @@ class User < ApplicationRecord
         :recoverable, :rememberable, :validatable
   
   has_many :boards, dependent: :destroy
+
+  def has_written?(board)
+    boards.exists?(id: board.id)
+  end
   
 end
