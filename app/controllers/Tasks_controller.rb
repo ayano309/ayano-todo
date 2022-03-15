7522a27f527e5b34dd1daf982324b95b693e0612
綾乃 <>
2022-03-15 19:16:57 +0900

    createアクション

--- app/controllers/Tasks_controller.rb
+++ app/controllers/Tasks_controller.rb
@@ -10,7 +10,24 @@ class TasksController < ApplicationController
     def new
         board = Board.find(params[:board_id])
         @task = board.tasks.build
+        @task.user = current_user
     end
 
+    def create
+        board = Board.find(params[:board_id])
+        @task = board.tasks.build(task_params)
+        @task.user = current_user
+        if @task.save
+            redirect_to board_path(board), notice: 'タスクを追加'
+        else
+            flash.now[:error] = '追加できませんでした'
+            render :new
+        end
+    end
     
+    private
+    def task_params
+        params.require(:task).permit(:name, :description)
+    end
+
 end
