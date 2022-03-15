adadb991ea9bf11c88f033bc89dadf174e479c85
綾乃 <>
2022-03-15 14:15:41 +0900

    has_many, belongs_to を記述,annotateの追加

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,22 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id          :integer          not null, primary key
+#  description :text
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  user_id     :integer
+#
+# Indexes
+#
+#  index_boards_on_user_id  (user_id)
+#
 class Board < ApplicationRecord
     validates :name, presence: true
     validates :description, presence: true
+
+    belongs_to :user
+
 end
