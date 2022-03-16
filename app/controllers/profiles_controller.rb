20804adc5999bfcd1dc8d1c6cda354432a746992
綾乃 <>
2022-03-16 16:09:11 +0900

    updateアクション

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,4 +9,18 @@ class ProfilesController < ApplicationController
     def edit
         @profile = current_user.build_profile
     end
+    def update
+        @profile = current_user.build_profile(profile_params)
+        if @profile.save
+            redirect_to profile_path, notice: 'プロフィール更新！'
+        else
+            flash.now[:error] = '更新できませんでした'
+            render :edit
+        end
+    end
+    
+    private
+    def profile_params
+        params.require(:profile).permit(:introduction)
+    end
 end
